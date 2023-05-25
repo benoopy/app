1a310595e453f5c223aa735ea1860fe588c8ee91
benoopy <dev.git@benoopy.com>
2023-05-25 09:34:16 +0200

    MOVE function location

--- assets/js/cookie-consent.js
+++ assets/js/cookie-consent.js
@@ -24,14 +24,20 @@ function getCookie(name) {
   
   function loadFacebookPixel() {
     if (getCookie('cookieConsent') !== 'accepted') return;
-    const script = document.createElement('script');
-    script.src = 'https://connect.facebook.net/en_US/fbevents.js';
-    document.getElementById('facebookPixel').appendChild(script);
-  
-    script.onload = function() {
-        fbq('init', '613901937331653');
-        fbq('track', 'PageView');
-    };
+    !function (f, b, e, v, n, t, s) {
+        if (f.fbq) return; 
+        n = f.fbq = function () {
+            n.callMethod ?
+                n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+        };
+        if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0';
+        n.queue = []; t = b.createElement(e); t.async = !0;
+        t.src = v; s = b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t, s)
+    }(window, document, 'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '613901937331653');
+    fbq('track', 'PageView');
   }
   
   window.onload = function() {
